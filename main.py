9b8e3837766a5372e16ca3d6a184eaa23a7eb830
Sebastian Fischer <s164158@student.dtu.dk>
2021-01-23 23:27:05 +0100

    main.py added to use DBA class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-
-
-
-
-
+from DBA import DBA
+import re
 if __name__ == '__main__':
-    print("h")
+    dba_con = DBA()
+    number_of_postings_retrieve = dba_con.get_number_of_postings()
+    print(number_of_postings_retrieve)
+
+    # request again and retrieve new number and calculate delta 
